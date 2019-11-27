c10183dfe7b1d1117ec50e85933cf7a4205a675a
NehaChaudhari274 <neharaut2793@gmail.com>
2019-11-27 13:16:27 +0530

    Add new Project for dev

---
+++ AAAAAAAA/src/com/wcs/A.java
@@ -0,0 +1,9 @@
+package com.wcs;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println(" hiiii git  demo");
+	}
+
+}
---
+++ AAAAAAAA/src/com/wcs/B.java
@@ -0,0 +1,5 @@
+package com.wcs;
+
+public class B {
+
+}
