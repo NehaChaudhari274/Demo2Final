d9051ec0b5da8a4926cbd5e0df7ad0d3c0628cc7
KIRAN-BORADE <kiranborhade123@gmail.com>
2019-11-27 15:03:37 +0530

    its new last code

--- AAAAAAAA/src/com/wcs/C.java
+++ AAAAAAAA/src/com/wcs/C.java
@@ -4,6 +4,8 @@ public class C {
 	
 	public static void main(String[] args) {
 		System.out.println("hiiiii...........kiran op");
+		System.out.println("hiiiii...........kiran op");
+		System.out.println("hiiiii...........kiran op");
 	}
 
 }
